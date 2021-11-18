26e981aab8cb35fcace0ad2f62aa924432654939
Keyi Zhang <keyi@stanford.edu>
2021-11-17 21:04:54 -0800

    add ninja codegen; need to test it out

--- src/codegen.cc
+++ src/codegen.cc
@@ -1,1 +1,72 @@
 #include "codegen.hh"
+
+#include <filesystem>
+#include <fstream>
+#include <set>
+
+#include "fmt/format.h"
+
+namespace xsim {
+
+inline std::string get_cc_filename(const std::string &name) { return fmt::format("{0}.cc", name); }
+
+void get_defs(const Module *module, std::set<std::string> &result) {
+    result.emplace(module->name);
+    for (auto const &[_, inst] : module->child_instances) {
+        get_defs(inst.get(), result);
+    }
+}
+
+std::set<std::string> get_defs(const Module *module) {
+    std::set<std::string> result;
+    get_defs(module, result);
+    return result;
+}
+
+void NinjaCodeGen::output(const std::string &path) {
+    std::ofstream stream(path, std::ios::trunc);
+    // filling out missing information
+    // use the output path as the runtime path
+    if (options_.runtime_path.empty()) {
+        auto p = std::filesystem::path(path);
+        options_.runtime_path = p.parent_path();
+    }
+    if (options_.clang_path.empty()) {
+        // hope for the best?
+        options_.clang_path = "clang";
+    }
+    if (options_.output_name.empty()) {
+        options_.output_name = default_output_name;
+    }
+
+    std::filesystem::path dir = options_.runtime_path;
+    auto include_dir = dir / "include";
+    stream << "cflags = -I" << include_dir << " ";
+    if (options_.debug_build) {
+        stream << "-O0 -g ";
+    } else {
+        stream << "-O3";
+    }
+    stream << std::endl << std::endl;
+    stream << "rule cc" << std::endl;
+    stream << "  depfile = $out.d" << std::endl;
+    stream << "  command = " << options_.clang_path << " -MD -MF $out.d $cflags -c $in -o $out"
+           << std::endl
+           << std::endl;
+
+    // output for each module definition
+    auto defs = get_defs(top_);
+    std::string objs;
+    for (auto const &name : defs) {
+        auto obj_name = fmt::format("{0}.o", name);
+        stream << "build " << obj_name << ": cc " << get_cc_filename(name) << std::endl;
+        objs.append(obj_name).append(" ");
+    }
+    // build the main
+    stream << "rule main" << std::endl;
+    stream << "  " << options_.clang_path << "$in " << objs << "$cflags -o $out" << std::endl
+           << std::endl;
+    stream << "build " << options_.output_name << ": main " << main_name << std::endl;
+}
+
+}  // namespace xsim
--- src/codegen.hh
+++ src/codegen.hh
@@ -1,17 +1,38 @@
 #ifndef XSIM_CODEGEN_HH
 #define XSIM_CODEGEN_HH
+#include <utility>
+
 #include "ir.hh"
 
 namespace xsim {
 
-class CXXCodeGen {
+// module is an illegal name in SV. using it will guarantee that
+// there is no conflicts
+auto constexpr main_name = "module.cc";
+auto constexpr default_output_name = "xsim.out";
 
+class CXXCodeGen {};
+
+struct NinjaCodeGenOptions {
+    bool debug_build = false;
+    std::string runtime_path;
+    std::string clang_path;
+    std::string output_name;
 };
 
 class NinjaCodeGen {
     // generates native ninja code (bypass cmake)
+public:
+    NinjaCodeGen(const Module *top, NinjaCodeGenOptions option)
+        : top_(top), options_(std::move(option)) {}
+
+    void output(const std::string &path);
+
+private:
+    const Module *top_;
+    NinjaCodeGenOptions options_;
 };
 
-}
+}  // namespace xsim
 
 #endif  // XSIM_CODEGEN_HH
